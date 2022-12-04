157da02c995efb17a89968041d189b90f77f3d59
MatheusRdC <carvalhoribeiro09@gmail.com>
2022-12-04 14:57:50 -0300

    feat: button

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <div className="container">
+      <input type="checkbox" id="checkbox-menu" />
+
+      <label htmlFor="checkbox-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+    </div>
+  );
+}
